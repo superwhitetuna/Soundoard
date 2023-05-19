c171762e9af02b190e3821936adc93f9b9efe1db
superwhitetuna <csulei@outlook.com>
2023-05-18 20:56:43 -0700

    Cleaned comments out

--- Soundoard.py
+++ Soundoard.py
@@ -1,54 +1,3 @@
-# import msvcrt
-# import pyaudio
-# import wave
-# import threading
-
-
-# def play_audio(filename):
-   
-#     chunk = 2048  
-
-#     wf = wave.open(filename, 'rb')
-
-#     p = pyaudio.PyAudio()
-
-#     stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
-#                     channels = wf.getnchannels(),
-#                     rate = wf.getframerate(),
-#                     output = True)
-    
-#     micstream = p.open(format = pyaudio.paInt16,
-#                     channels = 1,
-#                     rate = 44100,
-#                     input = True,
-#                     input_device_index=1)
-
-#     data = wf.readframes(chunk)
-    
-#     while data != '':
-#         stream.write(data)
-#         micstream.read(data)
-#         data = wf.readframes(chunk)
-
-#     stream.close()
-
-
-# User_Input = ""
-
-# while True:
-    
-#     if msvcrt.kbhit():
-#         key = msvcrt.getch()
-#         User_Input = key.decode("utf-8")
-#         print(User_Input)
-#         if User_Input == "1":
-#             filname = 'tunamayo.wav'
-
-#             # start playing audio in separate thread
-#             t = threading.Thread(target=play_audio, args=(filname,))
-#             t.start()
-
-
 import msvcrt
 import pyaudio
 import wave
@@ -58,12 +7,6 @@ def play_audio(filename):
     chunk = 2048  
     wf = wave.open(filename, 'rb')
     p = pyaudio.PyAudio()
-    # micstream = p.open(format=pyaudio.paInt16,
-    #                    channels=1,
-    #                    rate=44100,
-    #                    input=True,
-    #                    frames_per_buffer=chunk,
-    #                    input_device_index=1)
     print(p.get_default_output_device_info())
     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                     channels=wf.getnchannels(),
@@ -72,7 +15,6 @@ def play_audio(filename):
     data = wf.readframes(chunk)
     while data != '':
         stream.write(data)
-        # micstream.read(chunk)
         data = wf.readframes(chunk)
     stream.close()
 
