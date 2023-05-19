35cc4bf27bdde4ab80ca9eb089b3018d3ef76b4d
superwhitetuna <csulei@outlook.com>
2023-05-18 23:07:14 -0700

    Updates

--- Soundoard.py
+++ Soundoard.py
@@ -2,21 +2,50 @@ import msvcrt
 import pyaudio
 import wave
 import threading
+import time
+
+p = pyaudio.PyAudio()
+
+def get_vbaudiocable_device_info():
+    for i in range(p.get_device_count()):
+        current_device = p.get_device_info_by_index(i)
+        if current_device["name"] == "CABLE Input (VB-Audio Virtual C":
+            return current_device
 
 def play_audio(filename):
-    chunk = 2048  
-    wf = wave.open(filename, 'rb')
-    p = pyaudio.PyAudio()
-    print(p.get_default_output_device_info())
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True)
-    data = wf.readframes(chunk)
-    while data != '':
-        stream.write(data)
-        data = wf.readframes(chunk)
-    stream.close()
+    with wave.open(filename, 'rb') as wf:
+    # Define callback for playback (1)
+        def callback(in_data, frame_count, time_info, status):
+            data = wf.readframes(frame_count)
+            # If len(data) is less than requested frame_count, PyAudio automatically
+            # assumes the stream is finished, and the stream stops.
+            return (data, pyaudio.paContinue)
+
+        # Instantiate PyAudio and initialize PortAudio system resources (2)
+        p = pyaudio.PyAudio()
+
+        vb = get_vbaudiocable_device_info()
+
+        # Open stream using callback (3)
+        stream = p.open(format=pyaudio.paInt24,
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True,
+                        output_device_index=vb["index"],
+                        stream_callback=callback)
+
+        # Wait for stream to finish (4)
+        while stream.is_active():
+            time.sleep(0.1)
+
+        # Close the stream (5)
+        stream.close()
+
+        # Release PortAudio system resources (6)
+        p.terminate()
+
+
+print(get_vbaudiocable_device_info())
 
 User_Input = ""
 while True:
@@ -26,5 +55,4 @@ while True:
         print(User_Input)
         if User_Input == "1":
             filename = 'tunamayo.wav'
-            t = threading.Thread(target=play_audio, args=(filename,))
-            t.start()
+            play_audio(filename)
